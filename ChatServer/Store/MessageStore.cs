a84fb9608ad4a0899182945eea8202d1c6a982d9
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-11-10 09:35:36 +0100

    add skeleton for MessageStore

---
+++ ChatServer/Store/MessageStore.cs
@@ -0,0 +1,21 @@
+using ChatServer.Models;
+
+namespace ChatServer.Store;
+
+public class MessageStore
+{
+  public void Add(ChatMessage message)
+  {
+    throw new NotImplementedException();
+  }
+
+  public List<ChatMessage> GetAll()
+  {
+    throw new NotImplementedException();
+  }
+
+  public void Remove(int messageId)
+  {
+    throw new NotImplementedException();
+  }
+}
