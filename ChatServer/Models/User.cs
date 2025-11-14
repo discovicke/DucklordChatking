71a7c4718624d1bc90ebf0ce44533cdaf36328a6
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-11-14 09:36:32 +0100

    add Admin bool for User (future implementation of authorization stuff)

--- ChatServer/Models/User.cs
+++ ChatServer/Models/User.cs
@@ -1,9 +1,10 @@
 ﻿namespace ChatServer.Models;
 
-public class User(string username, string password)
+public class User(string username, string password, bool isAdmin)
 {
   public int Id { get; set; }
   public string Username { get; set; } = username;
   public string Password { get; set; } = password;
-  public string AuthToken { get; set; } = Guid.NewGuid().ToString();
+  public bool IsAdmin { get; set; } = isAdmin;
+  public string SessionAuthToken { get; set; } = Guid.NewGuid().ToString();
 }
