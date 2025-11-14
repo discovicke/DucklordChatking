270435b36e758ccff329e91e3a4994b2ca1b5e86
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-11-14 13:42:44 +0100

    update User model to accept external session token

--- ChatServer/Models/User.cs
+++ ChatServer/Models/User.cs
@@ -1,10 +1,10 @@
 ﻿namespace ChatServer.Models;
 
-public class User(string username, string password, bool isAdmin)
+public class User(string username, string password, bool isAdmin, string sessionAuthToken)
 {
   public int Id { get; set; }
   public string Username { get; set; } = username;
   public string Password { get; set; } = password;
   public bool IsAdmin { get; set; } = isAdmin;
-  public string SessionAuthToken { get; set; } = Guid.NewGuid().ToString();
+  public string SessionAuthToken { get; set; } = sessionAuthToken;
 }
