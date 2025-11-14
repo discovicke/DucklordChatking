f9579647e7e949501137b68e85116dcbc2eda15a
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-11-14 15:56:06 +0100

    style, clean up main program

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -5,26 +5,25 @@ using Microsoft.AspNetCore.SignalR;
 using Scalar.AspNetCore;
 using ChatServer.Auth;
 using ChatServer.Configuration;
+using ChatServer.Endpoints;
 
+// Stores (in-memory storage)
+UserStore userStore = new();
+MessageStore messageStore = new(userStore);
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSignalR(); // Register the SignalR service
 builder.Services.AddCustomOpenApi(); // Register the OpenAPI custom configuration which is found in the OpenApiConfiguration.cs Class
-
 var app = builder.Build();
 
-// Stores
-UserStore userStore = new();
-MessageStore messageStore = new(userStore);
-
-// Add one user for general testing
+// Seed initial users (for testing + Scalar UI authentication)
 userStore.Add("Ducklord", "chatking", isAdmin: true);
+userStore.Add("Scalar", "APIDOCS", isAdmin: true); // This user is used to allow Scalar UI to send test requests
 
-// This user is used to allow Scalar UI to send test requests
-userStore.Add("Scalar", "APIDOCS", isAdmin: true);
-Console.WriteLine(userStore.GetByUsername("Scalar")?.SessionAuthToken ?? "Token retrieval error: user not found or no token assigned. Ask server admin to generate one"); // Debug token print
+// Print debug token
+Console.WriteLine(userStore.GetByUsername("Scalar")?.SessionAuthToken ?? "Token retrieval error: user not found or no token assigned. Ask server admin to generate one");
 
-// Configure the web host, only works on localhost
+// Run the server locally on port 5201
 builder.WebHost.UseUrls("http://localhost:5201");
 
 // Scalar and OpenAPI are open even in production now to make debugging easier
