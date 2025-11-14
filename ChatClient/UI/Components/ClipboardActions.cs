938fc00aeb653737ce13f1f49962992de24a45e9
Marcus Löf <LeafMaster1@outlook.com>
2025-11-14 12:32:25 +0100

    if checks for clipboardactions commands

--- ChatClient/UI/Components/ClipboardActions.cs
+++ ChatClient/UI/Components/ClipboardActions.cs
@@ -21,6 +21,32 @@ namespace ChatClient.UI.Components
     public class ClipboardActionsClass 
     {
 
+        public static void BoardActions()
+        {
+            ClipboardAction action = ClipboardAction.None;
+            bool ctrlDown = Raylib.IsKeyDown(KeyboardKey.LeftControl) || Raylib.IsKeyDown(KeyboardKey.RightControl);
+            if (ctrlDown)
+            {
+                if (Raylib.IsKeyPressed(KeyboardKey.C))
+                {
+                    action = ClipboardAction.Copy;
+                }
+                else if (Raylib.IsKeyPressed(KeyboardKey.V))
+                {
+                    action = ClipboardAction.Paste;
+                }
+                else if (Raylib.IsKeyPressed(KeyboardKey.X))
+                {
+                    action = ClipboardAction.Cut;
+                }
+                else if (Raylib.IsKeyPressed(KeyboardKey.Z))
+                {
+                    action = ClipboardAction.Undo;
+                }
+            }
+
+            
+        }
 
     }
 
