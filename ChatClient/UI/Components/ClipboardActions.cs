65704469d7e8ea96d69d5aae4535d1a434b19c4d
Marcus Löf <LeafMaster1@outlook.com>
2025-11-14 12:26:50 +0100

    crated class for ClipBoardAction win en public enum for copy,paste logic

---
+++ ChatClient/UI/Components/ClipboardActions.cs
@@ -0,0 +1,27 @@
+﻿using ChatClient.Core;
+using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ChatClient.UI.Components
+{
+    public enum ClipboardAction
+    {
+        None,
+        Copy,
+        Paste,
+        Cut,
+        Undo
+    }
+
+    public class ClipboardActionsClass 
+    {
+
+
+    }
+
+}
