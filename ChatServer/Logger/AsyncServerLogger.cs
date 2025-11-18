6516424b5f66dc65ba615d83c90e4a6f685a5dc3
Christian Gennari <34597557+Christian-Gennari@users.noreply.github.com>
2025-11-18 14:12:15 +0100

    add ServerLogLevel enum for server logging severity levels

---
+++ ChatServer/Logger/AsyncServerLogger.cs
@@ -0,0 +1,17 @@
+using System.Collections.Concurrent;
+using System.Globalization;
+
+namespace ChatServer.Logger
+{
+  public enum ServerLogLevel
+  {
+    Trace,
+    Info,
+    Warning,
+    Success,
+    Error,
+    Critical
+  }
+
+
+}
